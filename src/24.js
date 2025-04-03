1ad8320ef993fa1c2b9393e6b9f9ab093a344b93
cromedyjackeline792 <cromedyjackeline792@gmail.com>
2025-04-03 13:19:11 +0000

    Add 24.js

---
+++ src/24.js
@@ -0,0 +1,12 @@
+let nodes = [
+  {
+    name: "Node1",
+    description: "Description of Node1"
+  },
+  {
+    name: "Node2",
+    description: "Description of Node2"
+  }
+];
+
+console.log(nodes);
